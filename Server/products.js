b5fc1c691182c45b51fc0c1ab2eb80dcb403226f
Niwesh Dhital <np01cp4a210205@islingtoncollege.edu.np>
2024-03-24 18:05:08 +0545

    Mental Health

---
+++ Server/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Depression",
+    desc: "Annals of depression",
+    image:
+      "https://res.cloudinary.com/dznnejowc/image/upload/v1708759073/MentalHealth/pexels-pixabay-207129_nevsoz.jpg",
+  },
+  {
+    id: 2,
+    name: "Anxiety",
+    desc: "Anxious woman in agony",
+    image:
+      "https://res.cloudinary.com/dznnejowc/image/upload/v1708759162/MentalHealth/pexels-ana-bregantin-1930523_hgygxj.jpg",
+  },
+  {
+    id: 3,
+    name: "Anxiety",
+    desc: "Anxious person screaming in agony",
+    image:
+      "https://res.cloudinary.com/dznnejowc/image/upload/v1708759162/MentalHealth/pexels-ana-bregantin-1930523_hgygxj.jpg",
+  },
+];
+
+module.exports = products;
